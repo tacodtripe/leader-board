b4bc23c6d4a0f64e98842706d4859da2b42fe656
tacodtripe <55043949+tacodtripe@users.noreply.github.com>
2021-11-03 14:51:58 -0700

    add css file to src

--- webpack.config.js
+++ webpack.config.js
@@ -11,5 +11,14 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
